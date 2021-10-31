57c500191884a8984d7142a7a9a8658260869b46
windyinmay <quynhanhmay.butter@gmail.com>
2021-11-01 01:22:23 +0200

    search function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { CardList } from "./components/card-lists/card-list";
+import { SearchBox } from "./components/search/search-box";
 import "./App.css";
 
 function App() {
   const [monster, setMonster] = useState([]);
-
+  const [input, setInput] = useState("");
+  const inputChange = (e) => {
+    setInput(e.target.value);
+  };
+  const filteredMonsters = monster.filter((m) =>
+    m.name.toLowerCase().includes(input.toLocaleLowerCase())
+  );
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((results) => setMonster(results));
+      .then((results) => setMonster(results))
+      .catch((err) => console.error(err));
   });
   return (
     <div className="App">
-      <CardList monster={monster}></CardList>
+      <SearchBox placeholder="Search monsters" handleChange={inputChange} />
+      <CardList monster={filteredMonsters}></CardList>
     </div>
   );
 }
